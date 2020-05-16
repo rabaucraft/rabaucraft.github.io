6421db738545c752febe8288b44722d1c3ab3a70
cumcraft <cumcraft@185-10-49-135.colo.transip.net>
2020-05-16 17:03:01 +0200

    test2

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": true,
-        "cacheTag": "1589639237",
+        "cacheTag": "1589641360",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world",
-            "last_rendertime": 1589638206,
+            "last_rendertime": 1589640912,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
