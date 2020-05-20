cc210b41e78215f53ffe8de3691fc68d361cdafa
cumcraft <cumcraft@185-10-49-135.colo.transip.net>
2020-05-20 15:23:52 +0200

    freshmap

--- overviewerConfig.js
+++ overviewerConfig.js
@@ -27,7 +27,7 @@ var overviewerConfig = {
     ],
     "map": {
         "debug": true,
-        "cacheTag": "1589894110",
+        "cacheTag": "1589976915",
         "north_direction": "lower-left",
         "controls": {
             "pan": true,
@@ -49,7 +49,7 @@ var overviewerConfig = {
             "base": "",
             "bgcolor": "#1a1a1a",
             "world": "world",
-            "last_rendertime": 1589893728,
+            "last_rendertime": 1589976264,
             "imgextension": "png",
             "isOverlay": false,
             "poititle": "Markers",
